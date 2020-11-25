0e9c0ba86653c1fa53e18ad280355f57a0681f95
이원진 <wj840612@gmail.com>
2020-11-25 20:30:17 +0900

    init

---
+++ src/main/java/com/woowa/board/WoowaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.woowa.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowaBoardApplication.class, args);
+	}
+
+}
