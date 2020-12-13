57167f82513795cd06398825c88749c6a6f50ffe
이원진 <wj840612@gmail.com>
2020-12-13 14:05:59 +0900

    필수값 체크 - Validation 로직 커밋시 누락

--- src/main/java/com/woowa/board/post/vo/PostRequest.java
+++ src/main/java/com/woowa/board/post/vo/PostRequest.java
@@ -1,5 +1,8 @@
 package com.woowa.board.post.vo;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,12 +12,16 @@ import lombok.ToString;
 @ToString
 public class PostRequest {
 
+	@NotNull(message = "게시판 정보가 존재하지 않습니다.")
 	private Long boardId;
 	
+	@NotBlank(message = "게시물 제목이 존재하지 않습니다.")
 	private String postTitle;
 	
+	@NotBlank(message = "게시물 내용이 존재하지 않습니다.")
 	private String postContent;
 	
+	@NotBlank(message = "등록자 정보가 존재하지 않습니다.")
 	private String userId;
 	
 }
