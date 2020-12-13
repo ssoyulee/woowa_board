64072ce2464ac956cec881f240ba89ebe5cb66b1
이원진 <wj840612@gmail.com>
2020-12-13 13:28:02 +0900

    댓글 등록 Asnyc로 변경  - 댓글 등록 시 Mail 전송으로 인해 속도 이슈가 발생하여 비동기 처리

--- src/main/java/com/woowa/board/noti/service/MailService.java
+++ src/main/java/com/woowa/board/noti/service/MailService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.woowa.board.noti.vo.RequestMail;
@@ -22,6 +23,7 @@ public class MailService {
     @Value("${spring.mail.username}")
     private String fromMail;
     
+    @Async("threadPoolTaskExecutor")
 	public void sendMail(RequestMail requestMail) throws MessagingException {
 
 		log.info("sendMail ::: requestMail => " + requestMail);
